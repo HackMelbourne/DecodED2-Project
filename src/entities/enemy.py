d48daf7bce1a425627823393408cf573c555dd40
Keith Leonardo <kleonardo52@gmail.com>
2021-06-27 20:46:30 +1000

    Fix enemy movement logic

--- src/entities/enemy.py
+++ src/entities/enemy.py
@@ -9,23 +9,21 @@ from src.entity import Entity
 # squares
 
 class Enemy(Entity):
-    turn = False  # Tells whether enemy needs to turn or not
-    direction = 1
-
     def __init__(self, coords):
         super().__init__(coords.x, coords.y, 20, 20, 'res/enemy.png')
-        self.velocity.x = ENEMY_SPEED
+        self.turn = False
+        self.direction = 1
 
     def tick(self, delta, objects):
+        # Jump a row once the screen border is hit 
+        # Updates static variable for all enemy instances
+        if self.boundary_check():
+            self.turn = True
+            
         if self.turn:
             self.direction *= -1
             self.velocity.y = ROW_JUMP_SIZE
             self.turn = False
         else:
             self.velocity.y = 0
-
-            # Jump a row once the screen border is hit 
-            # Updates static variable for all enemy instances
-            if self.boundary_check():
-                self.turn = True
         self.velocity.x = ENEMY_SPEED * self.direction
