84eb9f961904b7ee3ae2de0c47f10cf7cfb408c1
Keith Leonardo <kleonardo52@gmail.com>
2021-06-27 20:47:07 +1000

    Move shooting logic to player class and move generate_enemies to game class

--- src/entities/player.py
+++ src/entities/player.py
@@ -5,12 +5,13 @@ from pygame.locals import K_LEFT, K_RIGHT, KEYDOWN, KEYUP
 from src.constants import SCREEN_H, SCREEN_W, PLAYER_START_VECTOR, PLAYER_START_SPEED, BULLET_COOLDOWN
 from src.entity import Entity
 from src.entities.enemy import Enemy
-
+from src.entities.bullet import Bullet
 
 class Player(Entity):
     def __init__(self):
         super().__init__(PLAYER_START_VECTOR.x, PLAYER_START_VECTOR.y, 40, 40, 'res/player.png')
         self.move_direction = 0
+        self.shooting = False
         self.bullet_cooldown = BULLET_COOLDOWN
         self.speed = PLAYER_START_SPEED
 
@@ -23,8 +24,9 @@ class Player(Entity):
     def stop_moving(self):
         self.move_direction = 0
 
-    def can_shoot(self):
-        return self.bullet_cooldown == BULLET_COOLDOWN
+    def shoot(self):
+        if self.bullet_cooldown == BULLET_COOLDOWN:
+            self.shooting = True
 
     def tick(self, delta, objects):
         # Keep track of bullet cooldown
@@ -32,6 +34,12 @@ class Player(Entity):
             self.bullet_cooldown = BULLET_COOLDOWN
         else:
             self.bullet_cooldown += delta
+
+        # Shoot a bullet
+        if self.shooting:
+            objects.append(Bullet(Vector2(self.x, self.y)))
+            self.shooting = False
+            self.bullet_cooldown = 0
 
         # Set the movement velocity of the player based on its current direction
         self.velocity.x = self.move_direction * self.speed
--- src/game.py
+++ src/game.py
@@ -9,48 +9,42 @@ from src.entities.player import Player
 from src.entities.bullet import Bullet
 
 from src.entities.enemy import Enemy
-from src.constants import MAX_PER_ROW, ROW_GAP, SCREEN_W, TOTAL_GRID_SQUARES, INITIAL_NUM_ENEMIES, BULLET_COOLDOWN
+from src.constants import MAX_PER_ROW, ROW_GAP, SCREEN_W, TOTAL_GRID_SQUARES, INITIAL_NUM_ENEMIES
 
 BLACK = (0, 0, 0)
 
-
-def generate_enemies():
-    enemy_list = []
-    coords_check = []
-    OFFSET = 50
-
-    NUM_ROWS = TOTAL_GRID_SQUARES // MAX_PER_ROW
-    COL_GAP = SCREEN_W // MAX_PER_ROW
-
-    for i in range(NUM_ROWS):
-        coords_check.append([0 for x in range(MAX_PER_ROW)])
-
-    for num in range(INITIAL_NUM_ENEMIES):
-        rand_row = random.randint(0, NUM_ROWS - 1)
-        rand_col = random.randint(0, MAX_PER_ROW - 1)
-
-        # This loop is done to ensure that randomly initialised coordinates do not repeat
-        while True:
-            rand_row = random.randint(0, NUM_ROWS - 1)
-            rand_col = random.randint(0, MAX_PER_ROW - 1)
-            if coords_check[rand_row][rand_col] == 0:
-                coords_check[rand_row][rand_col] = 1
-                break
-
-        enemy_coords = Vector2(OFFSET + (rand_col * COL_GAP), OFFSET + (rand_row * ROW_GAP))
-        enemy_list.append(Enemy(enemy_coords))
-
-    return enemy_list
-
-
 class Game:
-    """Main game logic implementation"""
+    """Implementation of overall game logic"""
     def __init__(self):
         self.entities = []
         self.player = Player()
         self.entities.append(self.player)
-        for enemy in generate_enemies():
-            self.entities.append(enemy)
+        self.generate_enemies()
+
+    def generate_enemies(self):
+        coords_check = []
+        OFFSET = 50
+
+        NUM_ROWS = TOTAL_GRID_SQUARES // MAX_PER_ROW
+        COL_GAP = SCREEN_W // MAX_PER_ROW
+
+        for i in range(NUM_ROWS):
+            coords_check.append([0 for x in range(MAX_PER_ROW)])
+
+        for num in range(INITIAL_NUM_ENEMIES):
+            rand_row = random.randint(0, NUM_ROWS - 1)
+            rand_col = random.randint(0, MAX_PER_ROW - 1)
+
+            # This loop is done to ensure that randomly initialised coordinates do not repeat
+            while True:
+                rand_row = random.randint(0, NUM_ROWS - 1)
+                rand_col = random.randint(0, MAX_PER_ROW - 1)
+                if coords_check[rand_row][rand_col] == 0:
+                    coords_check[rand_row][rand_col] = 1
+                    break
+
+            enemy_coords = Vector2(OFFSET + (rand_col * COL_GAP), OFFSET + (rand_row * ROW_GAP))
+            self.entities.append(Enemy(enemy_coords))
 
     def update(self, delta, events):
         # Handle controls
@@ -61,16 +55,15 @@ class Game:
                 if event.key == K_RIGHT:
                     self.player.move_right()
                 if event.key == K_SPACE:
-                    if not self.player.expired and self.player.can_shoot():
-                        self.entities.append(Bullet(Vector2(self.player.x, self.player.y)))
-                        self.bullet_timer = 0
+                    if not self.player.expired:
+                        self.player.shoot()
             if event.type == KEYUP:
                 if event.key == K_LEFT and self.player.move_direction < 0:
                     self.player.stop_moving()
                 if event.key == K_RIGHT and self.player.move_direction > 0:
                     self.player.stop_moving()
 
-        # Loop through game objects and remove ones which are expired.      
+        # Loop through game objects and remove ones which are expired.
         # We are iterating backwards here
         for i in range(len(self.entities) - 1, -1, -1):
             # Kill expired entities
